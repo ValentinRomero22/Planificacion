2e8f1d7fde8c2893bd7cf4c91d162a4132ff12ce
Valentín Romero <valentin.romero@CENTRAL.ABITAB>
2026-01-08 18:12:07 -0300

    estilo grid 08.01

--- frontend/src/components/Grid.jsx
+++ frontend/src/components/Grid.jsx
@@ -36,40 +36,56 @@ export const Grid = () => {
     const days = createDays(new Date(2026, 0, 1), 30)
 
     return (
-        <section
-            className='gridContainer'
-            style={{
-                gridTemplateColumns: `150px repeat(${days.length}, 1fr)`
-            }}>
-            {/* HEADER DE LAS COLUMNAS */}
-            <div className='cellHeader userHeader'>USUARIO</div>
+        <section className='gridContainer'>
+            <section
+                className='grid'
+                style={{
+                    gridTemplateColumns: `150px repeat(${days.length}, 1fr)`
+                }}>
+                {/* HEADER DE LAS COLUMNAS */}
+                <div className='cellHeader userHeader'>USUARIO</div>
 
-            {
-                days.map(day => (
-                    <div
-                        key={day.id}
-                        className={`cellHeader ${day.isWeekend ? 'weekend' : 'weekday'}`}
-                    >
-                        {day.label}
-                    </div>
-                ))
-            }
-            {/* FILAS */}
-            {initialUsers.map(user => (
-                <>
-                    <div key={user.id} className='cellUser'>
-                        {user.user}
-                    </div>
-                    {
-                        days.map(day => (
-                            <div
-                                key={`${user.id}-${day.id}`}
-                                className={`cell ${day.isWeekend ? 'weekend' : 'weekday'}`}>
-                            </div>
-                        ))
-                    }
-                </>
-            ))}
+                {
+                    days.map(day => (
+                        <div
+                            key={day.id}
+                            className={`cellHeader ${day.isWeekend ? 'weekend' : 'weekday'}`}
+                        >
+                            {day.isWeekend ? "" : day.label}
+                        </div>
+                    ))
+                }
+                {/* FILAS */}
+                {initialUsers.map(user => (
+                    <>
+                        {/* SEPARADOR */}
+                        <div key={user.id} className='separatorCell'>
+                            ---
+                        </div>
+                        {
+                            days.map(day => (
+                                <div
+                                    key={`${user.id}-${day.id}`}
+                                    className='separatorCell'>
+                                </div>
+                            ))
+                        }
+                        
+                        {/* USUARIOS */}
+                        <div key={user.id} className='userCell'>
+                            {user.user}
+                        </div>
+                        {
+                            days.map(day => (
+                                <div
+                                    key={`${user.id}-${day.id}`}
+                                    className={`cell ${day.isWeekend ? 'weekend' : 'weekday'}`}>
+                                </div>
+                            ))
+                        }
+                    </>
+                ))}
+            </section>
         </section>
     )
 }
